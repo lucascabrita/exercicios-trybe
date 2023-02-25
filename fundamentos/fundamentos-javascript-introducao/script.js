79477630d929e5d077c28ad4db263c55bc8f1e89
Lucas Cabrita <lucasdsantosc@hotmail.com>
2023-02-25 16:04:52 -0300

    estudando objetos

---
+++ fundamentos/fundamentos-javascript-introducao/script.js
@@ -0,0 +1,18 @@
+// let num1 = 5;
+// let num2 = 15;
+//  if(num1 < num2) {
+//     console.log('numero um maior');
+//  }
+//  else
+//  console.log('numero dois maior');
+
+let car = {
+    type: 'Fiat',
+    model: '500',
+    color: 'white',
+  };
+  
+  for (let index in car) {
+    console.log(index, car[index]);
+
+  }
